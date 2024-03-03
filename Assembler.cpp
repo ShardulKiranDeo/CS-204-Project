f2212f3482900ac32e4224ee341585014a495410
ShardulKiranDeo <120590556+ShardulKiranDeo@users.noreply.github.com>
2024-03-03 16:02:30 +0530

    Update Assembler.cpp

--- Assembler.cpp
+++ Assembler.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <unordered_map>
+#include <fstream>
 using namespace std;
 
 unordered_map<string,string> opcode = {
@@ -35,12 +36,96 @@ unordered_map<string,string> opcode = {
  {"lui","0110111"},
  {"jal","1101111"}
 
+};
+
+unordered_map<string,string> registers={
+ {"x0","00000"},
+ {"x1","00001"},
+ {"x2","00010"},
+ {"x3","00011"},
+ {"x4","00100"},
+ {"x5","00101"},
+ {"x6","00110"},
+ {"x7","00111"},
+ {"x8","01000"},
+ {"x9","01001"},
+ {"x10","01010"},
+ {"x11","01011"},
+ {"x12","01100"},
+ {"x13","01101"},
+ {"x14","01110"},
+ {"x15","01111"},
+ {"x16","10000"},
+ {"x17","10001"},
+ {"x18","10010"},
+ {"x19","10011"},
+ {"x20","10100"},
+ {"x21","10101"},
+ {"x22","10110"},
+ {"x23","10111"},
+ {"x24","11000"},
+ {"x25","11001"},
+ {"x26","11010"},
+ {"x27","11011"},
+ {"x28","11100"},
+ {"x29","11101"},
+ {"x30","11110"},
+ {"x31","11111"}
+};
+
+void Rformat() //For R format instructions 
+{
+
+ 
+}
+
+
+void Iformat() //For I format instructions
+{
+
+ 
+}
+
+void Sformat() //For I format instructions
+{
+
+ 
+}
+
+
+void SBformat() //For SB format instructions
+{
+ 
+}
+
+void Uformat() //For U format instructions
+{
+ 
+}
+
+void UJformat() //For UJ format instructions
+{
+ 
+}
+
+
+void assemble(string inputf, string outputf) //Function to take input and write into the output file
+{
+   ifstream infile(inputf);
+   ofstream outfile(outputf);
+   
+   if(!infile.is_open() || !outfile.is_open())
+   {
+     cout<<"Error opening files"<<endl;
+     return ;
+   }
+
+   
 }
 
 int main()
 {
- //taking input through a file
+assemble("input.asm", "output.mc");
 
- // string parsing
   return 0;
 }
